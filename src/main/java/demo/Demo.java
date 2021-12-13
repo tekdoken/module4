87fd268bf1c4858da95a2db4da51c2de09060852
tekdoken <tekdokenzx@gmail.com>
2021-12-13 17:30:56 +0700

    tu dien

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class Demo {
     @GetMapping("/")
@@ -13,14 +18,34 @@ public class Demo {
         return "index";
     }
 
-    @GetMapping("/sum")
+    @GetMapping("/dicForm")
+    public String greeting1() {
+        return "dictionary";
+    }
+
+    @GetMapping("/dictionary")
     public String greeting1(@RequestParam String name, Model model) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("hello", "xin chào");
+        map.put("good bye", "tạm biệt");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getKey().equals(name)){
+                model.addAttribute("name", entry.getValue());
+            }else{
+                model.addAttribute("name", "");
+            }
+        }
+        return "dictionary";
+    }
+
+    @GetMapping("/sum")
+    public String sum(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
         return "a";
     }
 
     @PostMapping("/sum1")
-    public String sum(@RequestParam Integer a,  Model model) {
+    public String sum1(@RequestParam Integer a, Model model) {
         model.addAttribute("sum", a * 22000);
         return "index";
     }
---
+++ src/main/webapp/WEB-INF/views/dictionary.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: z
+  Date: 12/13/2021
+  Time: 5:02 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/dictionary">
+    <input type="text" name="name">
+</form>
+<h1>${name}</h1>
+</body>
+</html>
